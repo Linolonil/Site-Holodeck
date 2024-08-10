e3454d89372c93a41aea2414ba7fc4666ed0a128
Lino Jorge <109708821+Linolonil@users.noreply.github.com>
2024-08-10 19:46:41 -0400

    arquivo pra chamada de img e listas

---
+++ src/assets/Blog/assets.jsx
@@ -0,0 +1,47 @@
+export const btnMore = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="50px"
+    height="50px"
+    viewBox="0 0 24 24"
+    className="stroke-current text-[#8419c5] transition-colors duration-300"
+    style={{ fill: 'none' }} // Remove fill if you only want to apply stroke color
+  >
+    <path
+      d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
+      strokeWidth="1.5"
+    ></path>
+    <path d="M8 12H16" strokeWidth="1.5"></path>
+    <path d="M12 16V8" strokeWidth="1.5"></path>
+  </svg>
+);
+
+export const btnClose = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="50px"
+    height="50px"
+    viewBox="0 0 24 24"
+    className="stroke-current text-[#8419c5] transition-colors duration-300"
+    style={{ fill: 'none' }} // Remove fill if you only want to apply stroke color
+  >
+    <path
+      d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
+      strokeWidth="1.5"
+    ></path>
+    <path d="M8 12H16" strokeWidth="1.5"></path>
+  </svg>
+);
+
+export const listCategory = [
+  { value: "artificial-intelligence" },
+  { value: "digital-media" },
+  { value: "cybersecurity" },
+  { value: "gadgets" },
+  { value: "internet-of-things" },
+  { value: "mobile-technology" },
+  { value: "software-development" },
+  { value: "virtual-reality" },
+  { value: "technology" },
+  { value: "tech-industry-news" },
+];
