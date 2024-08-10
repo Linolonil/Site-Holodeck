28864e410796fbc9b38e98778e5abad619f221ce
Lino Jorge <109708821+Linolonil@users.noreply.github.com>
2024-08-10 19:42:16 -0400

    chamada da api corrigida e criação do cache para os artigos

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,29 +1,56 @@
-// src/pages/blog.jsx
-// import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
-import { useState } from "react";
-import ArticleMain from "../components/ArticleMain.jsx";
-import ArticlesSection from "../components/ArticlesSection.jsx";
-import FilterSection from "../components/FilterSection.jsx";
+import { useEffect, useState } from "react";
+import ArticleMain from "../components/Layout/Blog/ArticleMain.jsx";
+import ArticlesSection from "../components/Layout/Blog/ArticlesSection.jsx";
+import FilterSection from "../components/Layout/Blog/FilterSection.jsx";
+import axios from "axios";
+
+
+// esse cache salva muito, pois reduz o numero de req do backend (temporario)
+const cache = {}; 
+
 
 const Blog = () => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [category, setCategory] = useState("news");
 
-  const [selectedCategory, setSelectedCategory] = useState('');
 
+  useEffect(() => {
+    if (category) {
+      if (cache[category]) {
+        setArticles(cache[category]);
+        setLoading(false);
+      } else {
+        setLoading(true);
+        axios
+          .get(`http://localhost:5001/api/articles/category/${category}`)
+          .then((response) => {
+            setArticles(response.data);
+            cache[category] = response.data; // Armazena a resposta no cache
+            setLoading(false);
+          })
+          .catch((error) => {
+            setError("Erro ao carregar artigos!");
+            console.log(error);
+            setLoading(false);
+          });
+      }
+    }
+  }, [category]);
 
   return (
     <div className="min-h-screen bg-transparent">
       {/* Seção de Destaque */}
-      <ArticleMain/>
+      <ArticleMain />
 
       {/* Seção de Artigos Recentes */}
       <section className="container mx-auto px-6 my-12">
-       
-
-        {/*Filtro dos Artigos*/}
-        <FilterSection />
+        {/* Filtro dos Artigos */}
+        <FilterSection setCategory={setCategory} />
 
         {/* Lista de Artigos */}
-        <ArticlesSection />
+        <ArticlesSection articles={articles} loading={loading} error={error} />
       </section>
     </div>
   );
