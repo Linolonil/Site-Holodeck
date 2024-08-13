6abb4a4937a22651184cd772bb7b74a44a527854
Elnata Correa <elnata_correa@hotmail.com>
2024-08-13 10:12:51 -0400

    atualizando a sesao de contatc

--- src/components/Layout/Blog/ArticlesSection.jsx
+++ src/components/Layout/Blog/ArticlesSection.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import PropTypes from "prop-types";
-import imgDefault from "../../../assets/logo-sem-fundo.png";
+import imgDefault from "../Blog/ArticleMain";
 function ArticlesSection({ articles, loading, error }) {
   const defaultImage = imgDefault;
   return (
