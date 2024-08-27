b0752964872023003c5c6f4db94f26c32a65b64e
lino jorge <linox999@gmail.com>
2024-08-27 15:43:55 -0400

    ajustes nas validações

--- holodeck-backend/middlewares/validations.js
+++ holodeck-backend/middlewares/validations.js
@@ -1,53 +1,52 @@
 const validator = require("validator");
 const CPFValidator = require("cpf-cnpj-validator");
-const fs = require('fs');
 
-const validations = (req, res, next) => {
-  const { name, email, password, cpf, username } = req.body;
-
-  // Função para deletar a imagem se houver um erro
-  const deleteImage = () => {
-    if (req.file) {
-      const path = `uploads/${req.file.filename}`;
-      fs.unlink(path, (err) => {
-        if (err) {
-          console.error('Erro ao deletar a imagem:', err);
-        }
-      });
+//validar se os campos estão vazios
+const validateEmptyFields = (fields) => {
+  for (const [key, value] of Object.entries(fields)) {
+    if (validator.isEmpty(value.trim())) {
+      return `O campo ${key} não pode ser vazio!`;
     }
-  };
-
-  // Validações
-  if (!name || !email || !password || !cpf || !username) {
-    deleteImage();
-    return res
-      .status(400)
-      .json({ success: false, message: "Todos os campos são obrigatórios!" });
   }
+  return null;
+};
 
-  if (!CPFValidator.cpf.isValid(cpf)) {
-    deleteImage();
-    return res.status(400).json({ success: false, message: "CPF é inválido" });
+// Função para validar o CPF
+const validateCpf = (cpf) => {
+  return CPFValidator.cpf.isValid(cpf) ? null : "CPF inválido";
+};
+
+// Função para validar o email
+const validateEmail = (email) => {
+  return validator.isEmail(email) ? null : "Por favor, insira um email válido";
+};
+
+// Função para validar a senha
+const validatePassword = (password) => {
+  return password.length >= 8 ? null : "Por favor, insira uma senha com no mínimo 8 caracteres";
+};
+
+// Função principal de validação
+const validations = async (req, res, next) => {
+  const { name, email, password, cpf, username } = req.body;
+  try {
+  // Validações básicas
+  const errors = [
+    validateEmptyFields({ nome:name, email, senha: password, cpf, login:username }),
+    validateCpf(cpf),
+    validateEmail(email),
+    validatePassword(password),
+  ].filter(error => error);
+
+  if (errors.length > 0) {
+    return res.status(400).json({ success: false, message: errors[0] });
   }
-
-  if (!validator.isEmail(email)) {
-    deleteImage();
-    return res
-      .status(400)
-      .json({ success: false, message: "Por favor, insira um email válido" });
+    // Se não houver conflitos, continua com a requisição
+    next();
+  } catch (error) {
+    console.error("Erro ao validar campos:", error);
+    res.status(500).json({ success: false, message: "Erro interno do servidor" });
   }
-
-  if (password.length < 8) {
-    deleteImage();
-    return res
-      .status(400)
-      .json({
-        success: false,
-        message: "Por favor, insira uma senha com no mínimo 8 caracteres",
-      });
-  }
-
-  next();
 };
 
 module.exports = validations;
